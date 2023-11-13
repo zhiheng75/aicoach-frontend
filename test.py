3651a408baa824f5e89867ceb96b0fac0bc23166
子非鱼 <17001100170@163.com>
2023-11-13 10:23:18 +0800

    delete test push

--- test.py
+++
@@ -1,9 +0,0 @@
-import openai
-openai.api_key = "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"   #将第二步获取的密钥填写到这里
-completion = openai.ChatCompletion.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "user", "content": "介绍一下广州塔"}
-  ]
-)
-print(completion.choices)
