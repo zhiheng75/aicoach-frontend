86fad12b20a8990c9486bad9f107ade4df197385
纪博学 <406750082@qq.com>
2024-02-28 14:36:06 +0800

    增加渠道

---
+++ lib/util/channel.dart
@@ -0,0 +1,9 @@
+// ignore: camel_case_types
+class Channel {
+  static String channelbaidu = "baidu";
+  static String channelxiaomi = "xiaomi";
+  static String channelqihu360 = "qihu360";
+  static String channelyingyongbao = "android";
+  static String channelhuawei = "huawei";
+  static String channelios = "ios";
+}
