37a97d6fb1db4ed849a20e575d7b43f25cfc297b
纪博学 <406750082@qq.com>
2024-06-24 21:30:10 +0800

    2.0.6

--- lib/person/person.dart
+++ lib/person/person.dart
@@ -840,7 +840,7 @@ class _PersonPageState extends State<PersonPage>
           menuItem(
             'person_guanyu',
             '关于我们',
-            'v2.0.5',
+            'v2.0.6',
             onPress: () => tapMenu(PersonalRouter.about),
           ),
 
